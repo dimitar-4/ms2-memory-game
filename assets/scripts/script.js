e105f1716cff0d1876db9ecbaaf4be8575c7d764
dimitar-4 <dcholakov4@gmail.com>
2020-11-05 13:50:04 +0000

    Added function for the theme buttons to change the background

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+function turnDay () {
+            document.body.style.backgroundImage = "url('../assets/images/light-theme-bg.jpg')";
+        }
+
+function turnNight () {
+            document.body.style.backgroundImage = "url('../assets/images/dark-theme-bg.jpg')";
+        }
