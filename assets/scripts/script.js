01b646d6e978363e741f92df6537f64c827bd674
dimitar-4 <dcholakov4@gmail.com>
2020-11-06 16:26:56 +0000

    Fixed Flip card function and added a const to duplicate the cards instead of writing them twice

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,139 +1,96 @@
 // Theme Buttons JS
-
-function turnDay () {
-            document.body.style.backgroundImage = "url('../assets/images/light-theme-bg.jpg')";
-        }
-
-function turnNight () {
-            document.body.style.backgroundImage = "url('../assets/images/dark-theme-bg.jpg')";
-        }
-
-// Game JS
-
-document.addEventListener("DOMContentLoaded", () => {
-    const cards = [
-    {
-        name: "Toyota",
-        imageSrc: "./assets/images/toyota-logo.png"
-    },
-    {
-        name: "Honda",
-        imageSrc: "./assets/images/honda-logo.png"
-    },
-    {
-        name: "Subaru",
-        imageSrc: "./assets/images/subaru-logo.png"
-    },
-    {
-        name: "Nissan",
-        imageSrc: "./assets/images/nissan-logo.png"
-    },
-    {
-        name: "Mazda",
-        imageSrc: "./assets/images/mazda-logo.png"
-    },
-    {
-        name: "Mitsubishi",
-        imageSrc: "./assets/images/mitsubishi-logo.png"
-    },
-    {
-        name: "Suzuki",
-        imageSrc: "./assets/images/suzuki-logo.png"
-    },
-    {
-        name: "Daihatsu",
-        imageSrc: "./assets/images/daihatsu-logo.png"
-    },
-    {
-        name: "Toyota",
-        imageSrc: "./assets/images/toyota-logo.png"
-    },
-    {
-        name: "Honda",
-        imageSrc: "./assets/images/honda-logo.png"
-    },
-    {
-        name: "Subaru",
-        imageSrc: "./assets/images/subaru-logo.png"
-    },
-    {
-        name: "Nissan",
-        imageSrc: "./assets/images/nissan-logo.png"
-    },
-    {
-        name: "Mazda",
-        imageSrc: "./assets/images/mazda-logo.png"
-    },
-    {
-        name: "Mitsubishi",
-        imageSrc: "./assets/images/mitsubishi-logo.png"
-    },
-    {
-        name: "Suzuki",
-        imageSrc: "./assets/images/suzuki-logo.png"
-    },
-    {
-        name: "Daihatsu",
-        imageSrc: "./assets/images/daihatsu-logo.png"
-    }
-];
-
-cards.sort(() => Math.random() - 0.5);
-
-const game = document.querySelector(".game");
-
-var cardsChosen = [];
-
-var cardsChosenId = [];
-
-var cardsWon = [];
-
-var resultDisplay = {};
-
-function gameBoard() {
-    for (let i = 0; i < cards.length; i++) {
-        var card = document.createElement("img");
-        card.setAttribute("src", "./assets/images/card-back.png")
-        card.setAttribute("data-id", i)
-        card.addEventListener("click", flipCard)
-        game.appendChild(card)
-    }
+function turnDay() {
+  document.body.style.backgroundImage =
+    "url('../assets/images/light-theme-bg.jpg')";
 }
-
-// Matches
-
-function checkForMatch() {
-    var cardMatch = document.querySelectorAll("img")
+function turnNight() {
+  document.body.style.backgroundImage =
+    "url('../assets/images/dark-theme-bg.jpg')";
+}
+// Game JS
+const cards = [
+  {
+    id: 1,
+    name: "Toyota",
+    imageSrc: "./assets/images/toyota-logo.png",
+  },
+  {
+    id: 2,
+    name: "Honda",
+    imageSrc: "./assets/images/honda-logo.png",
+  },
+  {
+    id: 3,
+    name: "Subaru",
+    imageSrc: "./assets/images/subaru-logo.png",
+  },
+  {
+    id: 4,
+    name: "Nissan",
+    imageSrc: "./assets/images/nissan-logo.png",
+  },
+  {
+    id: 5,
+    name: "Mazda",
+    imageSrc: "./assets/images/mazda-logo.png",
+  },
+  {
+    id: 6,
+    name: "Mitsubishi",
+    imageSrc: "./assets/images/mitsubishi-logo.png",
+  },
+  {
+    id: 7,
+    name: "Suzuki",
+    imageSrc: "./assets/images/suzuki-logo.png",
+  },
+  { id: 8, name: "Daihatsu", imageSrc: "./assets/images/daihatsu-logo.png" },
+];
+const cardsWithDuplicates = cards.concat(cards);
+var cardsChosen = [];
+var cardsChosenId = [];
+var cardsWon = [];
+var resultDisplay = {};
+document.addEventListener("DOMContentLoaded", () => {
+  cardsWithDuplicates.sort(() => Math.random() - 0.5);
+  const game = document.querySelector(".game");
+  function initialiseGame() {
+    for (let i = 0; i < cardsWithDuplicates.length; i++) {
+      var card = document.createElement("img");
+      card.setAttribute("src", "./assets/images/card-back.png");
+      card.setAttribute("data-id", cardsWithDuplicates[i].id);
+      card.addEventListener("click", flipCard);
+      game.appendChild(card);
+    }
+  }
+  // Matches
+  function checkForMatch() {
+    var cardMatch = document.querySelectorAll("img");
     const cardOneId = cardsChosenId[0];
     const cardTwoId = cardsChosenId[1];
-    if (cardsChosen[0] === cardsChosen[1]) {
-        cardMatch[cardOneId].setAttribute("src")
-        cardMatch[cardTwoId].setAttribute("src")
-        cardsWon.push(cardsChosen)
-    } else {
-        cardMatch[cardOneId].setAttribute("src", "../assets/images/card-back.png")
-        cardMatch[cardTwoId].setAttribute("src", "../assets/images/card-back.png")
+    if (cardOneId !== cardTwoId) {
+      cardsChosen[0].setAttribute("src", "./assets/images/card-back.png");
+      cardsChosen[1].setAttribute("src", "./assets/images/card-back.png");
     }
     cardsChosen = [];
     cardsChosenId = [];
-    resultDisplay.textContent = cardsWon.length
-    if (cardsWon.length === cards.length/2) {
-        resultDisplay.textContent = "YOU WIN!"
+    resultDisplay.textContent = cardsWon.length;
+    if (cardsWon.length === cards.length / 2) {
+      resultDisplay.textContent = "YOU WIN!";
     }
-}
-
-// Flip Cards
-
-function flipCard() {
-    var cardId = this.dataset.id;
-    cardsChosen.push(cards[cardId].name)
-    cardsChosenId.push(cardId)
-    this.setAttribute("src", cards[cardId].img)
+  }
+  // Flip Cards
+  function flipCard() {
+    var cardId = parseInt(this.dataset.id);
+    cardsChosenId.push(cardId);
+    cardsChosen.push(this);
+    const imgSrc = cards.filter((obj) => {
+      return obj.id === cardId;
+    })[0].imageSrc;
+    this.setAttribute("src", imgSrc);
     if (cardsChosen.length === 2) {
-        setTimeout(checkForMatch, 500)
+      setTimeout(checkForMatch, 500);
     }
-}
-
-gameBoard()
-})
-
+  }
+  initialiseGame();
+});
